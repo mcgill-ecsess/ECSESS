c7243a6567574501bda74a305baf1b7e693b0f0b
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-06-05 11:11:26 +0100

    CMS fetching on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,62 @@
 <script>
-	import Section from 'components/Section.svelte';
+    import Section from 'components/Section.svelte';
 
-	// Getting info from ECSESS CMS
-	// let description =
-	// 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-	const query = '_type:homepage';
+    // Getting info from ECSESS CMS
+    // let description =
+    // 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-	let description = $state();
-	description = fetch(
-		`https://${import.meta.env.SANITY_ID}.api.sanity.io/vX/data/query/production?query=${query}&perspective=drafts`
-	).then((res) => res.json).then((json) => json.toString());
+    import { createClient } from '@sanity/client';
+
+    const client = createClient({
+        projectId: 'vmtsvpe2',
+        dataset: 'production',
+        useCdn: true,
+        apiVersion: '2025-02-06'
+    });
+
+    let description = $state('');
+    // const query = ;
+    async function getPosts() {
+        return await client.fetch('*[_type == "homepage"]');
+    }
 </script>
 
 <title> McGill ECSESS </title>
 
 <!-- ECSESS Introduction -->
 <Section>
-	<div class="flex h-1/2 flex-col items-center justify-center text-center">
-		<p class="page-title">What is ECSESS?</p>
-
-		<p>{description}</p>
-	</div>
+    <div class="flex h-1/2 flex-col items-center justify-center text-center">
+        <p class="page-title">What is ECSESS?</p>
+        {#await getPosts()}
+            <p>Loading...</p>
+        {:then o}
+            <p>{description}</p>
+            {#await o}
+                <p>text: {o}</p>
+            {:then text}
+                <p>desc: {o} {text}</p>
+            {/await}
+        {:catch error}
+            <blockquote>ERROR: {error}</blockquote>
+        {/await}
+    </div>
 </Section>
 
 <!-- Picture, FAQ -->
 <Section black>
-	<div class="flex justify-around gap-12">
-		<div>
-			<p>PICTURES</p>
-		</div>
-		<div>
-			<p>FAQ</p>
-		</div>
-	</div>
+    <div class="flex justify-around gap-12">
+        <div>
+            <p>PICTURES</p>
+        </div>
+        <div>
+            <p>FAQ</p>
+        </div>
+    </div>
 </Section>
 
 <!-- Office Hours Calendar -->
 <Section>
-	<div>
-		<p class="text-2xl">Office Hours</p>
-	</div>
+    <div>
+        <p class="text-2xl">Office Hours</p>
+    </div>
 </Section>
