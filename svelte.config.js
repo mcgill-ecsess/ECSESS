d220d4df5f02659758b0920421f385e691fa7d03
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-06-14 14:39:42 -0400

    Revert to adapter-auto for now

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,19 @@
 import { mdsvex } from 'mdsvex';
 // import adapter from '@sveltejs/adapter-node';
 // import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 		alias: {
-            // So you can import with `from 'components/...'`
-            'components': 'src/components/',
+      // So you can import with `from 'components/...'`
+      'components': 'src/components/',
 			'assets': 'src/assets',
 			'utils': 'src/utils'
         }
 	},
-
-  out: 'build',
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
 };
