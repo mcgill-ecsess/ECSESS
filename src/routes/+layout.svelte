bdb3a0613be8cf7424f6fc425cc395b9e3f0c7de
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-08-11 11:24:29 -0400

    Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
-    import '../app.css';
+	import '../app.css';
 	import Navbar from 'components/NavBar.svelte';
 	import Footer from 'components/Footer.svelte';
-    
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: 'auto' });
 	let { children } = $props();
 </script>
 
