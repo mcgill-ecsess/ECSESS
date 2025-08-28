f28e2ddceb9ce2523c305743fc06803e12a7e9ab
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-08-28 18:22:15 -0400

    Fix Event tabs CSS classses and create Tab Control component

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,138 +1,142 @@
 <script lang="ts">
 	import Section from 'components/Section.svelte';
-	import EventBlock from "components/EventBlock.svelte"
-	import type { EventPost } from '../../lib/schemas';
-    import { Tabs } from '@skeletonlabs/skeleton-svelte';
-	// import Tabs from "components/Tabs.svelte"
-	
-	let { data } = $props();
-	console.log(data)
+	import EventBlock from 'components/EventBlock.svelte';
+	import type { EventPost } from '$lib/schemas';
+	import { Tabs } from '@skeletonlabs/skeleton-svelte';
 
-	let events: EventPost[] = []
+	import EventTabControl from 'components/EventTabControl.svelte';
+	let { data } = $props();
+	console.log(data);
+
+	let events: EventPost[] = [];
 	data.events.forEach((event: EventPost) => {
-		events.push(event)
-	})
-    let group = $state('allEvents');
+		events.push(event);
+	});
+	let group = $state('allEvents');
 </script>
 
 <title> ECSESS Events </title>
 <Section>
 	<p class="page-title">Events</p>
-    <Tabs value={group} onValueChange={(e) => (group = e.value)} fluid>
-        {#snippet list()}
-            <Tabs.Control value="allEvents">
-            All Events
-            </Tabs.Control>
-            <Tabs.Control value="academic">
-            Academic
-            </Tabs.Control>
-            <Tabs.Control value="professional">
-            Professional
-            </Tabs.Control>
-            <Tabs.Control value="social">
-            Social
-            </Tabs.Control>
-            <Tabs.Control value="technical">
-            Technical
-            </Tabs.Control>
-        {/snippet}
-        {#snippet content()}
-            <Tabs.Panel value="allEvents">
-                <div class="event">
-                {#each events as e}
-                <EventBlock
-                    eventTitle={e.name} 
-                    date={e.date}
-                    location={e.location}
-                    eventDescription={e.description}
-                    thumbnail={e.thumbnail}
-                    registrationLink={e.reglink}
-                    paymentLink={e.paylink}
-                    eventCategory={e.category}
-                ></EventBlock>
-                {/each}
-            </div>
-            </Tabs.Panel>
-            <Tabs.Panel value="academic">
-            <div class="event">
-                {#each events as e}
-                {#if Array.isArray(e.category) 
-                    ? e.category.includes("academic") 
-                    : e.category === "academic"}
-                    <EventBlock
-                        eventTitle={e.name} 
-                        date={e.date}
-                        location={e.location}
-                        eventDescription={e.description}
-                        thumbnail={e.thumbnail}
-                        registrationLink={e.reglink}
-                        paymentLink={e.paylink}
-                        eventCategory={e.category}
-                    ></EventBlock>
-                {/if}
-                {/each}
-            </div>
-            </Tabs.Panel>
-            <Tabs.Panel value="professional">
-            <div class="event">
-                {#each events as e}
-                {#if Array.isArray(e.category) 
-                    ? e.category.includes("professional") 
-                    : e.category === "professional"}
-                    <EventBlock
-                        eventTitle={e.name} 
-                        date={e.date}
-                        location={e.location}
-                        eventDescription={e.description}
-                        thumbnail={e.thumbnail}
-                        registrationLink={e.reglink}
-                        paymentLink={e.paylink}
-                        eventCategory={e.category}
-                    ></EventBlock>
-                {/if}
-                {/each}
-            </div>
-            </Tabs.Panel>
-            <Tabs.Panel value="social">
-            <div class="event">
-                {#each events as e}
-                {#if Array.isArray(e.category) 
-                    ? e.category.includes("social") 
-                    : e.category === "social"}
-                    <EventBlock
-                        eventTitle={e.name} 
-                        date={e.date}
-                        location={e.location}
-                        eventDescription={e.description}
-                        thumbnail={e.thumbnail}
-                        registrationLink={e.reglink}
-                        paymentLink={e.paylink}
-                        eventCategory={e.category}
-                    ></EventBlock>
-                {/if}
-                {/each}
-            </div>
-            </Tabs.Panel>
-            <Tabs.Panel value="technical">
-                <div class="event">
-                {#each events as e}
-                {#if Array.isArray(e.category) 
-                    ? e.category.includes("technical") 
-                    : e.category === "technical"}
-                    <EventBlock
-                        eventTitle={e.name} 
-                        date={e.date}
-                        location={e.location}
-                        eventDescription={e.description}
-                        thumbnail={e.thumbnail}
-                        registrationLink={e.reglink}
-                        paymentLink={e.paylink}
-                        eventCategory={e.category}
-                    ></EventBlock>
-                {/if}
-                {/each}
-            </div>
-            </Tabs.Panel>
-        {/snippet}
-    </Tabs>
+	<Tabs value={group} onValueChange={(e) => (group = e.value)}>
+		{#snippet list()}
+			<Tabs.Control
+				value="allEvents"
+				classes="hover:border-b-ecsess-200 border-b-4 transition-all ease-in-out pb-2 text-lg active:border-b-ecsess-600"
+				stateActive="border-b-ecsess-400"
+			>
+				All Events
+			</Tabs.Control>
+			<Tabs.Control
+				value="academic"
+				classes="hover:border-b-ecsess-200 border-b-4 transition-all ease-in-out pb-2 text-lg  active:border-b-ecsess-600"
+				stateActive="border-b-ecsess-400"
+				>Academic
+			</Tabs.Control>
+
+			<Tabs.Control
+				value="professional"
+				classes="hover:border-b-ecsess-200 border-b-4 transition-all ease-in-out pb-2 text-lg  active:border-b-ecsess-600"
+				stateActive="border-b-ecsess-400"
+				>Professional
+			</Tabs.Control>
+			<EventTabControl value="social">Social</EventTabControl>
+			<Tabs.Control
+				value="technical"
+				classes="hover:border-b-ecsess-200 border-b-4 transition-all ease-in-out pb-2 text-lg  active:border-b-ecsess-600"
+				stateActive="border-b-ecsess-400"
+				>Technical
+			</Tabs.Control>
+		{/snippet}
+		{#snippet content()}
+			<Tabs.Panel value="allEvents">
+				<div class="event">
+					{#each events as e}
+						<EventBlock
+							eventTitle={e.name}
+							date={e.date}
+							location={e.location}
+							eventDescription={e.description}
+							thumbnail={e.thumbnail}
+							registrationLink={e.reglink}
+							paymentLink={e.paylink}
+							eventCategory={e.category}
+						></EventBlock>
+					{/each}
+				</div>
+			</Tabs.Panel>
+			<Tabs.Panel value="academic">
+				<div class="event">
+					{#each events as e}
+						{#if Array.isArray(e.category) ? e.category.includes('academic') : e.category === 'academic'}
+							<EventBlock
+								eventTitle={e.name}
+								date={e.date}
+								location={e.location}
+								eventDescription={e.description}
+								thumbnail={e.thumbnail}
+								registrationLink={e.reglink}
+								paymentLink={e.paylink}
+								eventCategory={e.category}
+							></EventBlock>
+						{/if}
+					{/each}
+				</div>
+			</Tabs.Panel>
+			<Tabs.Panel value="professional">
+				<div class="event">
+					{#each events as e}
+						{#if Array.isArray(e.category) ? e.category.includes('professional') : e.category === 'professional'}
+							<EventBlock
+								eventTitle={e.name}
+								date={e.date}
+								location={e.location}
+								eventDescription={e.description}
+								thumbnail={e.thumbnail}
+								registrationLink={e.reglink}
+								paymentLink={e.paylink}
+								eventCategory={e.category}
+							></EventBlock>
+						{/if}
+					{/each}
+				</div>
+			</Tabs.Panel>
+			<Tabs.Panel value="social">
+				<div class="event">
+					{#each events as e}
+						{#if Array.isArray(e.category) ? e.category.includes('social') : e.category === 'social'}
+							<EventBlock
+								eventTitle={e.name}
+								date={e.date}
+								location={e.location}
+								eventDescription={e.description}
+								thumbnail={e.thumbnail}
+								registrationLink={e.reglink}
+								paymentLink={e.paylink}
+								eventCategory={e.category}
+							></EventBlock>
+						{/if}
+					{/each}
+				</div>
+			</Tabs.Panel>
+			<Tabs.Panel value="technical">
+				<div class="event">
+					{#each events as e}
+						{#if Array.isArray(e.category) ? e.category.includes('technical') : e.category === 'technical'}
+							<EventBlock
+								eventTitle={e.name}
+								date={e.date}
+								location={e.location}
+								eventDescription={e.description}
+								thumbnail={e.thumbnail}
+								registrationLink={e.reglink}
+								paymentLink={e.paylink}
+								eventCategory={e.category}
+							></EventBlock>
+						{/if}
+					{/each}
+				</div>
+			</Tabs.Panel>
+		{/snippet}
+	</Tabs>
 </Section>
