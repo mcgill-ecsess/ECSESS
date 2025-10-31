a72f265289b2496a2a2927935694b53f88e16d55
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-10-31 08:37:45 -0400

    Start milk mile

---
+++ src/routes/milkmile/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Section from "components/layout/Section.svelte";
+
+</script>
+<Section from="from-ecsess-black" to="to-ecsess-black" via="via-ecsess-800">
+    <p class="page-title">Milk Mile Leaderboard</p>
+    <div>
+    </div>
+</Section>
