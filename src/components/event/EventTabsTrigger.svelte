19b955eeab8653f0662e90c689434111d991ec2c
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-12-19 22:07:08 -0500

    Prettier lint a file

--- src/components/event/EventTabsTrigger.svelte
+++ src/components/event/EventTabsTrigger.svelte
@@ -7,7 +7,7 @@
 	{#snippet element(attributes)}
 		<button
 			{...attributes}
-			class="hover:border-b-ecsess-200 border-b-4 border-b-transparent transition-all ease-in-out pb-2 text-lg px-2 data-[state=active]:border-b-ecsess-400"
+			class="hover:border-b-ecsess-200 data-[state=active]:border-b-ecsess-400 border-b-4 border-b-transparent px-2 pb-2 text-lg transition-all ease-in-out"
 		>
 			{@render children()}
 		</button>
