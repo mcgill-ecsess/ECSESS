36632b025fea8b08f89322d962718492bff2141c
Minh Vo <minh.vo2@mail.mcgill.ca>
2025-12-27 00:38:27 -0500

    remove peeking effect for smaller devices and add back shadow to the card

--- src/components/event/EventBlock.svelte
+++ src/components/event/EventBlock.svelte
@@ -61,13 +61,13 @@
 <div
 	class="
 	flip-box
-	group relative flex flex-col overflow-hidden rounded-2xl  transition-all duration-300 hover:-translate-y-3 "
+	group relative flex flex-col rounded-2xl  transition-all duration-300 hover:-translate-y-3"
 	>
 	<!--Flip Card container-->
-	<div class="flip-box-inner rounded-2xl " class:show-back={showDescription}>
+	<div class="flip-box-inner rounded-2xl  " class:show-back={showDescription}>
 		<!--Front Side-->
 		<!--Opacity thing is to fix the visual bug on ios-->
-		<div class="flip-box-front bg-ecsess-950 shadow-ecsess-950/50 rounded-2xl transition-opacity duration-500 {showDescription ? 'opacity-0 pointer-events-none' : 'opacity-100'}">
+		<div class="flip-box-front bg-ecsess-950 shadow-xl shadow-ecsess-950/60 rounded-2xl transition-opacity duration-500 {showDescription ? 'opacity-0 pointer-events-none' : 'opacity-100'}">
 			<!--Flip button-->
 			<div
 				class="
@@ -239,7 +239,7 @@
 			</div>
 			</div>
 		<!-- Back Side -->
-		<div class=" flip-box-back bg-ecsess-950 shadow-ecsess-950/50 rounded-2xl">
+		<div class=" flip-box-back bg-ecsess-950 shadow-xl shadow-ecsess-950/60 rounded-2xl">
 			<!--Flip button-->
 			<div
 				class="
